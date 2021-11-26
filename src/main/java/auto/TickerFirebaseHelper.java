b125d74f9082bc9dc3dc7cecae602c3aee91a88d
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2021-11-26 12:45:31 +0530

    bugs fixed

---
+++ src/main/java/auto/TickerFirebaseHelper.java
@@ -0,0 +1,91 @@
+package auto;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.ContentType;
+import javax.mail.internet.MimeMultipart;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class TickerFirebaseHelper {
+
+    public static boolean hasDuplicate(String s) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> tickets = db.collection("tickets").get();
+        List<QueryDocumentSnapshot> documents = tickets.get().getDocuments();
+        for (QueryDocumentSnapshot doc : documents) {
+            if (s.equals(doc.getString("sender"))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String getTextFromMessage(Message message) throws IOException, MessagingException {
+        String result = "";
+        if (message.isMimeType("text/plain")) {
+            result = message.getContent().toString();
+        } else if (message.isMimeType("multipart/*")) {
+            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+            result = getTextFromMimeMultipart(mimeMultipart);
+        }
+        return result;
+    }
+
+    private static String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart) throws IOException, MessagingException {
+
+        int count = mimeMultipart.getCount();
+        if (count == 0)
+            throw new MessagingException("Multipart with no body parts not supported.");
+        boolean multipartAlt = new ContentType(mimeMultipart.getContentType()).match("multipart/alternative");
+        if (multipartAlt)
+            // alternatives appear in an order of increasing
+            // faithfulness to the original content. Customize as req'd.
+            return getTextFromBodyPart(mimeMultipart.getBodyPart(count - 1));
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            result += getTextFromBodyPart(bodyPart);
+        }
+        return result;
+    }
+
+    private static String getTextFromBodyPart(
+            BodyPart bodyPart) throws IOException, MessagingException {
+
+        String result = "";
+        if (bodyPart.isMimeType("text/plain")) {
+            result = (String) bodyPart.getContent();
+        } else if (bodyPart.isMimeType("text/html")) {
+            String html = (String) bodyPart.getContent();
+            result = org.jsoup.Jsoup.parse(html).text();
+        } else if (bodyPart.getContent() instanceof MimeMultipart) {
+            result = getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent());
+        }
+        return result;
+    }
+
+    private static void updateOldMessageCount(int i) {
+        Firestore db = FirestoreClient.getFirestore();
+        try {
+            DocumentReference docRef = db.collection("settings").document("statics");
+            Map<String, Object> data = new HashMap<>();
+            data.put("lastReadMailCount", i);
+            ApiFuture<WriteResult> result = docRef.set(data);
+            System.out.println("Update time : " + result.get().getUpdateTime());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/main/InboxCountTester.java
+++ src/main/java/main/InboxCountTester.java
@@ -1,5 +1,6 @@
 package main;
 
+import auto.TickerFirebaseHelper;
 import categorizer.EmailCheck.MailCategorizer;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
@@ -11,11 +12,8 @@ import email.listener.EmailListener;
 import email.listener.EmailListenerUtil;
 import firebase.FirebaseUtil;
 
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.internet.ContentType;
-import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,7 +42,7 @@ public class InboxCountTester {
                 final Message[] messages = emailListener.getInbox().getMessages(oldMessageCount, newMessageCount); // get only new messages
                 for (int i = 0; i < messages.length; i++) {
 
-                    String messageBody = getTextFromMessage(messages[i]);
+                    String messageBody = TickerFirebaseHelper.getTextFromMessage(messages[i]);
                     String category = mailCategorizer.getCategory(messageBody, messages[i].getSubject());
                     String priority = mailCategorizer.getPriority(messageBody);
                     if (!priority.equals("1")) {
@@ -76,11 +74,11 @@ public class InboxCountTester {
         try {
             Firestore db = FirestoreClient.getFirestore();
 
-            boolean hasDuplicate = hasDuplicate(message.getHeader("Message-ID")[0]);
+            boolean hasDuplicate = TickerFirebaseHelper.hasDuplicate(message.getHeader("Message-ID")[0]);
             if (!hasDuplicate) {
                 Map<String, Object> data = new HashMap<>();
                 data.put("subject", message.getSubject());
-                data.put("body", getTextFromMessage(message));
+                data.put("body", TickerFirebaseHelper.getTextFromMessage(message));
                 data.put("from", message.getFrom()[0].toString());
                 data.put("sender", message.getHeader("Message-ID")[0]);
                 data.put("category", category);
@@ -97,76 +95,4 @@ public class InboxCountTester {
         }
     }
 
-    private static boolean hasDuplicate(String s) throws ExecutionException, InterruptedException {
-        Firestore db = FirestoreClient.getFirestore();
-        ApiFuture<QuerySnapshot> tickets = db.collection("tickets").get();
-        List<QueryDocumentSnapshot> documents = tickets.get().getDocuments();
-        for (QueryDocumentSnapshot doc : documents) {
-            if (s.equals(doc.getString("sender"))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    private static String getTextFromMessage(Message message) throws IOException, MessagingException {
-        String result = "";
-        if (message.isMimeType("text/plain")) {
-            result = message.getContent().toString();
-        } else if (message.isMimeType("multipart/*")) {
-            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
-            result = getTextFromMimeMultipart(mimeMultipart);
-        }
-        return result;
-    }
-
-    private static String getTextFromMimeMultipart(
-            MimeMultipart mimeMultipart) throws IOException, MessagingException {
-
-        int count = mimeMultipart.getCount();
-        if (count == 0)
-            throw new MessagingException("Multipart with no body parts not supported.");
-        boolean multipartAlt = new ContentType(mimeMultipart.getContentType()).match("multipart/alternative");
-        if (multipartAlt)
-            // alternatives appear in an order of increasing
-            // faithfulness to the original content. Customize as req'd.
-            return getTextFromBodyPart(mimeMultipart.getBodyPart(count - 1));
-        String result = "";
-        for (int i = 0; i < count; i++) {
-            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-            result += getTextFromBodyPart(bodyPart);
-        }
-        return result;
-    }
-
-    private static String getTextFromBodyPart(
-            BodyPart bodyPart) throws IOException, MessagingException {
-
-        String result = "";
-        if (bodyPart.isMimeType("text/plain")) {
-            result = (String) bodyPart.getContent();
-        } else if (bodyPart.isMimeType("text/html")) {
-            String html = (String) bodyPart.getContent();
-            result = org.jsoup.Jsoup.parse(html).text();
-        } else if (bodyPart.getContent() instanceof MimeMultipart) {
-            result = getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent());
-        }
-        return result;
-    }
-
-    private static void updateOldMessageCount(int i) {
-        Firestore db = FirestoreClient.getFirestore();
-        try {
-            DocumentReference docRef = db.collection("settings").document("statics");
-            Map<String, Object> data = new HashMap<>();
-            data.put("lastReadMailCount", i);
-            ApiFuture<WriteResult> result = docRef.set(data);
-            System.out.println("Update time : " + result.get().getUpdateTime());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
 }
