e7247408d91244249835cf79ce7f4589ed89cdcb
Luchn1k <pasindukalana@yahoo.com>
2021-11-26 14:25:01 +0530

    sdasdasd

--- src/main/java/main/InboxCountTester.java
+++ src/main/java/main/InboxCountTester.java
@@ -12,46 +12,63 @@ import email.config.property.PropertyFactory;
 import email.listener.EmailListener;
 import email.listener.EmailListenerUtil;
 import firebase.FirebaseUtil;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.mail.Message;
 
-
 public class InboxCountTester {
+
     public static void main(String[] args) {
-        // TODO: runs on startup
+
         MailCategorizer mailCategorizer = new MailCategorizer();
-        EmailListener emailListener = new EmailListener(new EmailConfig(PropertyFactory.getConfig(EmailProperty.IMAP)).getSession());
+
         try {
-            emailListener.init();
 
-            int newMessageCount = emailListener.getInbox().getMessageCount();
             FirebaseUtil.initializeApp();
-            Firestore db = FirestoreClient.getFirestore();
-            ApiFuture<DocumentSnapshot> query = db.collection("settings").document("statics").get();
-            DocumentSnapshot querySnapshot = query.get();
+        } catch (Exception e) {
+        }
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                EmailListener emailListener = new EmailListener(new EmailConfig(PropertyFactory.getConfig(EmailProperty.IMAP)).getSession());
+                try {
+                    emailListener.init();
+                    int newMessageCount = emailListener.getInbox().getMessageCount();
+                    Firestore db = FirestoreClient.getFirestore();
+                    ApiFuture<DocumentSnapshot> query = db.collection("settings").document("statics").get();
+                    DocumentSnapshot querySnapshot = query.get();
 
-            int oldMessageCount = querySnapshot.get("lastReadMailCount", Integer.class); // TODO: implement db
+                    int oldMessageCount = querySnapshot.get("lastReadMailCount", Integer.class); // TODO: implement db
 
-            if (oldMessageCount < newMessageCount) {
-                final Message[] messages = emailListener.getInbox().getMessages(oldMessageCount, newMessageCount);
-                for (int i = 0; i < messages.length; i++) {
+                    if (oldMessageCount < newMessageCount) {
+                        final Message[] messages = emailListener.getInbox().getMessages(oldMessageCount, newMessageCount);
+                        for (int i = 0; i < messages.length; i++) {
 
-                    String messageBody = TicketFirebaseHelper.getTextFromMessage(messages[i]);
-                    String category = mailCategorizer.getCategory(messageBody, messages[i].getSubject());
-                    // TODO: send them to categorizer
-                    String priority = TicketFirebaseHelper.getEmailPriority(db, messageBody, mailCategorizer);
-                    TicketFirebaseHelper.addEmailToFireBase(messages[i], category, priority);
+                            String messageBody = TicketFirebaseHelper.getTextFromMessage(messages[i]);
+                            String category = mailCategorizer.getCategory(messageBody, messages[i].getSubject());
+                            // TODO: send them to categorizer
+                            String priority = TicketFirebaseHelper.getEmailPriority(db, messageBody, mailCategorizer);
+                            TicketFirebaseHelper.addEmailToFireBase(messages[i], category, priority);
 
-                    TicketFirebaseHelper.updateOldMessageCount(oldMessageCount + i);
-                    // TODO: oldMessageCount = oldMessageCount+i ; // update the oldCount after adding email to db
+                            TicketFirebaseHelper.updateOldMessageCount(oldMessageCount + i);
+                            // TODO: oldMessageCount = oldMessageCount+i ; // update the oldCount after adding email to db
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    EmailListenerUtil.closeFolder(emailListener.getInbox());
+                    EmailListenerUtil.closeStore(emailListener.getStore());
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            EmailListenerUtil.closeFolder(emailListener.getInbox());
-            EmailListenerUtil.closeStore(emailListener.getStore());
-        }
+        }, 0, 2000);
+
+    }
+
+    private static void initListner() {
+
     }
 
 }
